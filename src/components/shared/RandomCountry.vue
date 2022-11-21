7a93cbec792ee0a0eab7d8665dbfb50545144aff
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-11-21 17:24:56 +0300

    minor changes

--- src/components/shared/RandomCountry.vue
+++ src/components/shared/RandomCountry.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="grid xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1">
+  <div
+    class="grid grid-cols-1 xl:grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1"
+  >
     <HeaderComponent></HeaderComponent>
-
-    <div class="flex flex-col justify-center items-center bg-stone-600">
-      <div class="grid grid-flow-row grid-cols-1 bg-orange-500">
+    <div
+      class="flex flex-col justify-center items-center bg-stone-600 rounded-lg"
+    >
+      <div class="m-4">
         <div
-          class="flex justify-start items-center hover:cursor-pointer bg-red-600"
+          v-if="isAnswerTrue"
+          class="animate-bounce p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+          role="alert"
         >
-          <span class="flex flex-wrap">
-            <ion-icon
-              name="information-circle-outline"
-              size="large"
-              @click="showIcon = !showIcon"
-            ></ion-icon
-          ></span>
-          <span v-if="showIcon"
-            >Write capitals without spaces. Ex: Washington DC => WashingtonDC.
-            And the desired name is the common name. (En)
-          </span>
+          <span class="font-medium">Success! </span>Congratulation, you knew the
+          capital.
         </div>
+        <div
+          v-if="isAnswerWarn"
+          class="animate-bounce p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800"
+          role="alert"
+        >
+          <span class="font-medium">Wrong answer! </span> Try again.
+          {{ randomCountryCapitalLetterArray.length - countWrongAnswer }} left.
+        </div>
+        <div
+          v-if="isAnswerWrong || showCapital === true"
+          class="p-4 mb-4 text-xl text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+          role="alert"
+        >
+          <span class="font-medium"
+            >You lose!
+            <span class="text-purple-800 bg-pink-400 rounded-md p-1"
+              >{{ randomCountryCapital }}
+            </span></span
+          >
+          is the capital. It will be start again.
+        </div>
+      </div>
+
+      <div class="grid grid-flow-row grid-cols-1 bg-orange-500 rounded-lg">
         <div class="p-2 flex justify-start text-red-300">
           <span class="font-semibold text-sky-400">Country: </span
           >{{ randomCountryName }}
@@ -27,7 +47,7 @@
           <span class="font-semibold flex justify-start text-sky-400"
             >Pure Capital:
           </span>
-          <div v-if="!showCapital">
+          <div v-if="showCapital">
             {{ randomCountryCapital }}, Word Length:{{
               randomCountryCapitalLetterArray.length
             }}
@@ -35,21 +55,26 @@
         </div>
         <div class="p-2 flex justify-start flex-wrap text-red-300">
           <span class="font-semibold flex justify-start text-sky-400"
-            >Capital Array:</span
+            >Changed Capital:</span
           >
-          {{ randomCountryCapitalLetterArray }}
+          <div v-if="showCapital">
+            {{ randomCountryCapitalLetterJoin }}
+          </div>
         </div>
         <div class="p-2 flex justify-start flex-wrap text-red-300">
           <span class="font-semibold flex justify-start text-sky-400"
-            >Changed Capital:</span
+            >Capital Array:</span
           >
-          {{ randomCountryCapitalLetterJoin }}
+          <div v-if="showCapital">
+            {{ randomCountryCapitalLetterArray }}
+          </div>
         </div>
+
         <div class="p-2 flex justify-start flex-wrap text-red-300">
           <span class="font-semibold flex justify-start text-sky-400"
             >Score:</span
           >
-          {{ localTrueAnswer }}
+          {{ countTrueAnswer === 0 ? localTrueAnswer : countTrueAnswer }}
         </div>
         <div class="p-2 flex justify-start flex-wrap text-red-300">
           <span class="font-semibold flex justify-start text-sky-400"
@@ -69,7 +94,6 @@
           @keyup.enter="checkEnteredValue"
           @keyup="handleInputOnKeyup"
         />
-        <!-- <div>{{ enteredValue.toLocaleUpperCase() }}</div> -->
       </label>
 
       <div class="flex justify-center items-center mb-4">
@@ -89,18 +113,7 @@
         >
           {{ capital.toUpperCase() }}
         </div>
-      </div>
-
-      <!-- <div class="flex flex-row">
-        <div
-          class="box flex flex-wrap justify-center items-center text-black font-bold m-0.5"
-          v-for="(capital, idx) in randomCountryCapitalLetterArray"
-          :key="idx"
-          @click="showWord(idx)"
-        >
-          {{}}
-        </div>
-      </div> -->
+      </div> 
 
       <div class="flex flex-row">
         <div
@@ -112,9 +125,9 @@
         </div>
       </div>
 
-      <div class="flex">
+      <div class="flex justify-center bg-slate-400 m-2">
         <button
-          @click="showCapital = !showCapital"
+          @click="(showCapital = !showCapital), showCapitalFunc()"
           class="flex justify-center items-center m-2 p-2 bg-red-300 rounded-lg hover:cursor-pointer hover:bg-red-400"
         >
           Show Capital
@@ -148,18 +161,39 @@
           Refresh
         </button>
       </div>
-      <div>{{ randomCountryCapitalLetterArray }}</div>
-      <div>{{ randomCountryCapitalLetterJoin }}</div>
+      <!-- <div>{{ randomCountryCapitalLetterArray }}</div>
+      <div>{{ randomCountryCapitalLetterJoin }}</div> -->
       <div>{{ countWrongAnswer }}</div>
-    </div>
 
-    <hr />
-    <div>True Answer:{{ countTrueAnswer }}</div>
-    <div>Local True Answer:{{ localTrueAnswer }}</div>
+      <div class="flex justify-between items-center m-2">
+        <div class="bg-indigo-400 mr-3 p-2 rounded-md text-orange-800">
+          Score:{{ countTrueAnswer }}
+        </div>
+        <div class="bg-indigo-400 ml-3 p-2 rounded-md text-orange-800">
+          Last Score:{{ localTrueAnswer }}
+        </div>
+      </div>
+
+      <div
+        class="flex justify-center items-center flex-wrap bg-red-600 rounded-xl"
+      >
+        <div class="">
+          <ion-icon
+            name="information-circle-outline"
+            size="large"
+            class="text-white hover:text-zinc-900 cursor-pointer"
+            @click="showIcon = !showIcon"
+          ></ion-icon>
+          <div v-if="showIcon" class="text-white">
+            Write capitals without spaces. Ex: Washington DC => WashingtonDC.
+            And the desired name is the common name. (En) If refresh the page
+            the score will be reset. (En)
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script lang="ts"></script>
 
 <script setup lang="ts">
 import HeaderComponent from "../layout/HeaderComponent.vue";
@@ -169,6 +203,10 @@ import { ICountry } from "@/models/Country.interface";
 import { useStore } from "vuex";
 
 const store = useStore();
+
+const isAnswerTrue = ref(false);
+const isAnswerWrong = ref(false);
+const isAnswerWarn = ref(false);
 
 const countriesService = new CountriesService();
 const countries = ref<ICountry[]>([]);
@@ -196,23 +234,57 @@ const randomCountryCapitalLetterJoin = ref<string>();
 
 const checkEnteredValue = () => {
   if (enteredValue.value === randomCountryCapitalLetterJoin.value) {
-    alert("Correct");
+    isAnswerTrue.value = true;
+    isAnswerWrong.value = false;
+    isAnswerWarn.value = false;
     countWrongAnswer.value = 0;
+    enteredValue.value = "";
     countTrueAnswer.value++;
-    localStorage.setItem("score", countTrueAnswer.value);
+    /* setTimeout(() => {
+      refreshPage();
+    }, 1500); */
+    changeCapital();
+    localStorage.setItem("score", countTrueAnswer.value.toString());
   } else {
-    alert("Wrong");
+    isAnswerWarn.value = true;
+    isAnswerTrue.value = false;
     countWrongAnswer.value++;
     if (
       countWrongAnswer.value === randomCountryCapitalLetterArray.value.length
     ) {
-      alert("You lost. Try again");
+      isAnswerWrong.value = true;
+      isAnswerWarn.value = false;
+      setTimeout(() => {
+        refreshPage();
+      }, 1500);
       countWrongAnswer.value = 0;
       countTrueAnswer.value = 0;
-      localStorage.setItem("score", 0);
+      localStorage.setItem("score", countTrueAnswer.value.toString());
     }
   }
 };
+
+const showCapitalFunc = () => {
+  if (showCapital.value === true) {
+    isAnswerWarn.value = false;
+    isAnswerTrue.value = false;
+    isAnswerWrong.value = true;
+    setTimeout(() => {
+      refreshPage();
+    }, 1500);
+  }
+};
+
+/* const showOneWordFunc = () => {
+  if(randomCountryCapitalLetterArray.value.length ) {
+    isAnswerWarn.value = false;
+    isAnswerTrue.value = false;
+    isAnswerWrong.value = true;
+    setTimeout(() => {
+      refreshPage();
+    }, 1500);
+  }
+}; */
 
 const getRandomCountry = ref<string[]>([]);
 
@@ -241,6 +313,8 @@ const showWorde = (maxNr: number) => {
       return showWorde(maxNr);
     } else {
       console.log("No more numbers available.");
+      showCapital.value = true;
+      showCapitalFunc();
       return false;
     }
   }
@@ -248,10 +322,15 @@ const showWorde = (maxNr: number) => {
 
 //Change capital
 const changeCapital = () => {
-  randomCountryCapitalLetterArray.value =
-    randomCountryCapitalLetterJoin.value.split("");
-  randomCountryCapitalLetterArray.value =
-    randomCountryCapitalLetterArray.value.sort(() => Math.random() - 0.5);
+  randomCountryCapitalLetterArray.value = [];
+  randomCountryCapitalLetterJoin.value = "";
+  letterArray.value = [];
+  randomArray = [];
+  countWrongAnswer.value = 0;
+  isAnswerWarn.value = false;
+  isAnswerTrue.value = false;
+  isAnswerWrong.value = false;
+  getCountriesInfo();
 };
 
 //getCountriesInfo method.
@@ -282,18 +361,17 @@ const getCountriesInfo = () => {
 
 watch(randomCountryCapital, (newValue) => {
   randomCountryCapitalLetter.value = newValue.split("");
-  console.log(randomCountryCapitalLetter.value);
+  /* console.log(randomCountryCapitalLetter.value); */
   randomCountryCapitalLetter.value.map((item) => {
     randomCountryCapitalLetterArray.value.push(item);
   });
-  console.log("Word list:", randomCountryCapitalLetterArray.value);
+  /* console.log("Word list:", randomCountryCapitalLetterArray.value);
   console.log(
     "Word list length:",
     randomCountryCapitalLetterArray.value.length
-  );
-
+  ); */
   for (let i = 0; i < randomCountryCapitalLetterArray.value.length; i++) {
-    console.log(randomCountryCapitalLetterArray.value[i]);
+    /* console.log(randomCountryCapitalLetterArray.value[i]); */
     if (
       randomCountryCapitalLetterArray.value[i] === "É" ||
       randomCountryCapitalLetterArray.value[i] === "È" ||
@@ -302,7 +380,7 @@ watch(randomCountryCapital, (newValue) => {
       randomCountryCapitalLetterArray.value[i] === "Ē"
     ) {
       randomCountryCapitalLetterArray.value[i] = "E";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (
       randomCountryCapitalLetterArray.value[i] === "Å" ||
@@ -316,30 +394,30 @@ watch(randomCountryCapital, (newValue) => {
       randomCountryCapitalLetterArray.value[i] === "Ą"
     ) {
       randomCountryCapitalLetterArray.value[i] = "A";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (randomCountryCapitalLetterArray.value[i] === "Ó") {
       randomCountryCapitalLetterArray.value[i] = "O";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (randomCountryCapitalLetterArray.value[i] === "Ș") {
       randomCountryCapitalLetterArray.value[i] = "S";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (randomCountryCapitalLetterArray.value[i] === "Ñ") {
       randomCountryCapitalLetterArray.value[i] = "N";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (
       randomCountryCapitalLetterArray.value[i] === "İ" ||
       randomCountryCapitalLetterArray.value[i] === "Í"
     ) {
       randomCountryCapitalLetterArray.value[i] = "I";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (randomCountryCapitalLetterArray.value[i] === "Ý") {
       randomCountryCapitalLetterArray.value[i] = "Y";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
     if (
       randomCountryCapitalLetterArray.value[i] === "Û" ||
@@ -347,7 +425,18 @@ watch(randomCountryCapital, (newValue) => {
       randomCountryCapitalLetterArray.value[i] === "Ú"
     ) {
       randomCountryCapitalLetterArray.value[i] = "U";
-      console.log("Changed: ", randomCountryCapitalLetterArray.value[i]);
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
+    }
+    if (
+      randomCountryCapitalLetterArray.value[i] === "Ć" ||
+      randomCountryCapitalLetterArray.value[i] === "Ç" ||
+      randomCountryCapitalLetterArray.value[i] === "Č" ||
+      randomCountryCapitalLetterArray.value[i] === "Ĉ" ||
+      randomCountryCapitalLetterArray.value[i] === "Ċ" ||
+      randomCountryCapitalLetterArray.value[i] === "Č"
+    ) {
+      randomCountryCapitalLetterArray.value[i] = "C";
+      /* console.log("Changed: ", randomCountryCapitalLetterArray.value[i]); */
     }
   }
   randomCountryCapitalLetterJoin.value =
@@ -368,6 +457,25 @@ onMounted(() => {
   &:hover {
     background-color: #a16ae8;
     cursor: pointer;
+  }
+}
+@media only screen and (max-width: 640px) {
+  .box {
+    width: 25px;
+    height: 25px;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .box {
+    width: 30px;
+    height: 30px;
+  }
+}
+@media only screen and (max-width: 1024px) {
+  .box {
+    width: 35px;
+    height: 35px;
   }
 }
 </style>
